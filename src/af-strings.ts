04b6564183cc431421bb0ce611725be9d3f2e974
Avi <avidrucker@gmail.com>
2020-09-15 11:17:01 -0400

    update export app, debug, cla, etc. strings

---
+++ src/af-strings.ts
@@ -0,0 +1,19 @@
+// TODO: assess whether creating a JSON file instead is more appropriate for your needs
+// TODO: assess which strings are needed only for console (command line) app
+export const doneFocusing = 'Once you finish focusing on this task, tap the enter key.';
+export const skippingReview = `Skipping review (list is not reviewable)...`;
+export const emptyList = "There are no items in your to-do list.";
+export const makeMenuSelection = `Please make a menu selection.`;
+export const automarkingFirstMarkable = `Auto-marking first markable item...`;
+export const cantAutomark = `Unable to auto-mark. Returning list as is...`;
+export const cantFocus = `Cannot focus at this time. Mark (dot) an item first.`;
+export const readAboutApp = `This is a stub for About AutoFocus...`;
+export const errorReadingState = `It appears there was an error reading state...`;
+export const errorMutatingState = `It appears there was an error mutating state...`;
+export const setupDemoData = `Setting up starting demo data...`;
+export const demoDataComplete = `... demo data setup is complete.`;
+export const wantToHideCompleted = "Do you want to hide completed items? ";
+export const enterNewItem = `Please enter a to-do item: `;
+export const indexAtEndOfArr = `Index is at the end of the array: returning not found...`;
+export const notMarkableOrReviewable = `List is neither markable nor reviewable.`;
+export const cantMarkOrReviewBecauseNoItems = "Your list is empty. First, add some items.";
